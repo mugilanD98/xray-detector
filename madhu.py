9b88e4f8aa500edebd478bce15c1509db37b6957
mugilanD98 <73863531+mugilanD98@users.noreply.github.com>
2020-11-11 23:28:28 +0530

    Update madhu.py

--- madhu.py
+++ madhu.py
@@ -6,8 +6,8 @@ from PIL import Image
 import requests
 from io import BytesIO
 st.set_option('deprecation.showfileUploaderEncoding',False)
-st.title("xray")
-st.text("provide URL")
+st.title("radiology assistant")
+st.text("provide URL of x-ray to classify")
 @st.cache(allow_output_mutation=True)
 def load_model():
     model = tf.keras.models.load_model('pneumoniamugi.h5')
@@ -28,7 +28,7 @@ def decode_img(image):
     img=scale(img) 
     return np.expand_dims(img,axis=0)
 
-path = st.text_input('enter image url ','https://images.app.goo.gl/y8Nd9zX2u6pSMCW96')
+path = st.text_input('enter image url ','https://storage.googleapis.com/kagglesdsdata/datasets/17810/23812/chest_xray/test/NORMAL/IM-0001-0001.jpeg?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=databundle-worker-v2%40kaggle-161607.iam.gserviceaccount.com%2F20201110%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20201110T221420Z&X-Goog-Expires=172799&X-Goog-SignedHeaders=host&X-Goog-Signature=6e7109157b51c5e4ffcd090b0f731ddd86ed306afc19d9c769495cc515b390ece403b652c83d42a99f31194f1ba84afb6b3c55ad07f4839022617d7ce32d2489db6cd6f004bba6884ea1a801601daefc525916c5f66b7479a26e0b1ec2578eb4575559a61b49adbb36f5668e95e5375f3c5d83fbb4fdf47b9c11db7db2070c655f3f67646ef4c88371255797939a87bfd424e1329ff3620d70d3ce0e163353d41bb184df5e70cadc3c826256a1cd75ab6df3684f1ff1f7e01c7b608d96c37517d535b00a6f4fc28d4932c5357ac69c071f2d696867f8554d981f1e692bbe2e675b28efc91f850db7dbfa43287eee81053172fa5c9261eeb5a3dc81a65f441a03')
 if path is not None:
     content=requests.get(path).content
 
